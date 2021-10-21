5f2fdd919d6770322e366cde132a69ad2915fef1
Foxxx48 <foxxdim@mail.ru>
2021-10-21 15:05:34 +0300

    Delete array languages

--- src/main/java/com/fox/spring/mvc/Employee.java
+++ src/main/java/com/fox/spring/mvc/Employee.java
@@ -1,13 +1,33 @@
 package com.fox.spring.mvc;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
 public class Employee {
     private String name;
     private String surname;
     private int salary;
     private String department;
+    private  String carBrand;
+    private Map<String,String> carBrands;
+    private String languages;
+    private Map<String,String> languageList;
 
     public Employee() {
+        carBrands = new HashMap<>();
+        carBrands.put("BMW", "BMW");
+        carBrands.put("Tesla", "Tesla");
+        carBrands.put("Mercedes-Benz", "MB");
+
+        languageList = new HashMap<>();
+        languageList.put("English", "EN");
+        languageList.put("Deutch", "DE");
+        languageList.put("French", "FR");
     }
+
+
+
 
     public String getName() {
         return name;
@@ -41,6 +61,39 @@ public class Employee {
         this.department = department;
     }
 
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public Map<String, String> getCarBrands() {
+        return carBrands;
+    }
+
+    public void setCarBrands(Map<String, String> carBrands) {
+        this.carBrands = carBrands;
+    }
+
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+
+    public Map<String, String> getLanguageList() {
+        return languageList;
+    }
+
+    public void setLanguageList(Map<String, String> languageList) {
+        this.languageList = languageList;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
@@ -48,6 +101,10 @@ public class Employee {
                 ", surname='" + surname + '\'' +
                 ", salary=" + salary +
                 ", department='" + department + '\'' +
+                ", carBrand='" + carBrand + '\'' +
+                ", carBrands=" + carBrands +
+                ", languages=" + languages +
+                ", languageList=" + languageList +
                 '}';
     }
 }
